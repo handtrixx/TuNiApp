47a0e7ee1de1d9ca51a631eac773fde03e8308eb
handtrixxx <pooh85@gmx.de>
2020-12-17 11:39:35 +0100

    Version 1.0
    Feature Complete Release

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', async event => {
+   console.log('install event')
+ });
+ 
+ self.addEventListener('fetch', async event => {
+   console.log('fetch event')
+ });
+
+ const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+  './',
+  './black.png',
+  './bootstrap.bundle.min.js',
+  './bootstrap.bundle.min.js.map',
+  './bootstrap.min.css',
+  './bootstrap.min.css.map',
+  './icon_192.png',
+  './icon.ico',
+  './index.html',
+  './masonry.pkgd.min.js',
+  './OpenSans-Regular.woff',
+  './OpenSans-Regular.woff2'
+];
+
+self.addEventListener('install', async event => {
+   const cache = await caches.open(cacheName); 
+   await cache.addAll(staticAssets); 
+ });
